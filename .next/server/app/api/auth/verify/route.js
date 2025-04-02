4ef071b2bb47e2308e368e0134b243024fe2cfb2
Melanie Dohr <114560271+DohrMelanie@users.noreply.github.com>
2025-04-02 18:13:16 +0200

    Delete .next directory

--- .next/server/app/api/auth/verify/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/auth/verify/route.js";
-const runtime = require("../../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_4d3a3141._.js");
-runtime.loadChunk("server/chunks/[root of the server]__2f87524a._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/auth/verify/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/auth/verify/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/auth/verify/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/app/api/v1/me/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/v1/me/route.js";
-const runtime = require("../../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_5a780db8._.js");
-runtime.loadChunk("server/chunks/[root of the server]__7ff467d2._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/v1/me/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/v1/me/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/v1/me/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
